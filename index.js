68aa4acbb11d2fc215aa20672ac7100d503c28f1
Lekan Adeyeri <oadeyeri@students.pitzer.edu>
2021-01-12 04:26:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const Alexa = require('ask-sdk-core');
-const main = require('./src/main');
+const m = require('./src/main');
 const hh = require('./src/helperHandlers');
 const utih = require('./src/userTurnIntentHandlers');
 
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
-    main.LaunchRequestHandler, 
-    main.SessionEndedRequestHandler, 
+    m.LaunchRequestHandler, 
+    m.SessionEndedRequestHandler, 
     hh.ChangeThemeIntentHandler, 
     hh.HelpIntentHandler, 
     hh.RepeatIntentHandler, 
     hh.CancelAndStopIntentHandler, 
     utih.UserTurnIntentHandler)
-  .addErrorHandlers(main.ErrorHandler)
+  .addErrorHandlers(m.ErrorHandler)
   .lambda();
